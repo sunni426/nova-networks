6a1a84dfef9eb72dadcc8adce98935b9dd1759b5
shihch3 <chien-cheng.shih@novartis.com>
2024-02-27 20:54:38 -0800

    update the path in shell

--- preprocessing/mike/preprocessing.sh
+++ preprocessing/mike/preprocessing.sh
@@ -1,14 +1,16 @@
-SCRIPTDIR="/dlab/ldrive/CBT/USLJ-DSDE-I10007/DSDE/shihch3/code/p/python/nova-networks/preprocessing/mike"
+REPODIR="/dlab/ldrive/CBT/USLJ-DSDE-I10007/DSDE/shihch3/code/p/python/nova-networks"
 DATADIR="/dlab/ldrive/CBT/USLJ-DSDE_DATA-I10008/BenchmarkDatasets/hpa-single-cell-image-classification"
+OPDATADIR="/dlab/ldrive/CBT/USLJ-DSDE_DATA-I10008/shihch3/projects/HPA_single_data/code_test"
+
+SCRIPTDIR="${REPODIR}/preprocessing/mike"
 IPDIR="${DATADIR}/train"
 NCPU=10
 
-OPDATADIR="/dlab/ldrive/CBT/USLJ-DSDE_DATA-I10008/shihch3/projects/HPA_single_data/code_test"
 OPIMGDIR="${OPDATADIR}/train/img"
 OPIMGRGBDIR="${OPDATADIR}/train/imgRGB"
 OPSEGDIR="${OPDATADIR}/train/seg"
 OPCELLDIR="${OPDATADIR}/train/cell"
-CSVPATH="/dlab/ldrive/CBT/USLJ-DSDE-I10007/DSDE/shihch3/code/p/python/nova-networks/HPA-nova/dataloaders/split/train_sunni_toy.csv"
+CSVPATH="${REPODIR}/HPA-nova/dataloaders/split/train_sunni_toy.csv"
 FRAMESIZE=1024 # examples: "1024", "1024 1024", or "1024 512"
 
 python "${SCRIPTDIR}/channel_merge.py" \
@@ -43,7 +45,7 @@ OPIMGDIR="${OPDATADIR}/val/img"
 OPIMGRGBDIR="${OPDATADIR}/val/imgRGB"
 OPSEGDIR="${OPDATADIR}/val/seg"
 OPCELLDIR="${OPDATADIR}/val/cell"
-CSVPATH="/dlab/ldrive/CBT/USLJ-DSDE-I10007/DSDE/shihch3/code/p/python/nova-networks/HPA-nova/dataloaders/split/valid_sunni_toy.csv"
+CSVPATH="${REPODIR}/HPA-nova/dataloaders/split/valid_sunni_toy.csv"
 FRAMESIZE=1024 # examples: "1024", "1024 1024", or "1024 512"
 
 python "${SCRIPTDIR}/channel_merge.py" \
