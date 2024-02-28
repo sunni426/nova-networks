b9e2f02a2df9cd51f51fa6643b7304910c832485
shihch3 <chien-cheng.shih@novartis.com>
2024-02-27 21:54:25 -0800

    update cell export all

--- preprocessing/mike/crop_img.py
+++ preprocessing/mike/crop_img.py
@@ -35,9 +35,10 @@ def encode_binary_mask(mask: np.ndarray) -> t.Text:
     return base64_str.decode("utf-8")
 
 
-def find_valid_crops(image, masks, min_size=(100, 100), max_save=10):
+def find_valid_crops(image, masks, save_encode_mask, min_size=(100, 100), max_save=10):
     valid_crops = []
     file_number = 1
+    mask_data = []  # Store encoded masks with file names
     unique_masks = np.unique(masks)
     for mask_id in unique_masks:
         if mask_id == 0:
@@ -52,6 +53,11 @@ def find_valid_crops(image, masks, min_size=(100, 100), max_save=10):
         if (y_max - y_min + 1) < min_size[0] or (x_max - x_min + 1) < min_size[1]:
             continue
 
+        # Encode mask and store with file number
+        if save_encode_mask:
+            encoded_mask = encode_binary_mask(mask)
+            mask_data.append((file_number, encoded_mask))
+
         cropped_cell = np.zeros(
             (y_max - y_min + 1, x_max - x_min + 1, 3), dtype=np.uint8
         )  # RGB
@@ -64,33 +70,55 @@ def find_valid_crops(image, masks, min_size=(100, 100), max_save=10):
         valid_crops.append((cropped_cell, file_number))
         file_number += 1
 
-        if len(valid_crops) >= max_save:
-            break
+        if max_save != "all":
+            if len(valid_crops) >= max_save:
+                break
 
-    return valid_crops
+    if save_encode_mask:
+        return valid_crops, mask_data
+    else:
+        return valid_crops, None
 
 
-def save_crops(valid_crops, save_dir, orig_filename):
+def save_crops(valid_crops, save_dir, id):
     file_c_digit = len(str(len(valid_crops)))
     for cropped_image, file_number in valid_crops:
-        image_save_path = save_dir.parent.joinpath(
-            f"{orig_filename}_cell{str(file_number).zfill(file_c_digit)}.png"
+        image_save_path = save_dir.joinpath(
+            f"{id}_cell{str(file_number).zfill(file_c_digit)}.png"
         )
         imsave(image_save_path, cropped_image)
     return
 
 
-def _crop_image(id, ipimgdir, ipsegdir, opdir, max_cellc):
+def save_mask_data(mask_data, save_dir, id):
+    file_c_digit = len(str(len(mask_data)))
+    mask_data_file = save_dir.joinpath(f"{id}_masks.txt")
+    with open(mask_data_file, "w") as file:
+        for file_number, encoded_mask in mask_data:
+            file.write(f"{id}_cell{str(file_number.file_c_digit)}: {encoded_mask}\n")
+    return
+
+
+def _crop_image(id, ipimgdir, ipsegdir, opimgdir, opcodedir, max_cellc):
+    if opcodedir is not None:
+        save_encode_mask = True
+    else:
+        save_encode_mask = False
+
     image = imread(ipimgdir.joinpath(f"{id}.png"))
     masks = imread(ipsegdir.joinpath(f"{id}_cp_masks.png"))
-    opimgpath = opdir.joinpath(f"{id}.png")
-    valid_crops = find_valid_crops(image, masks, max_save=max_cellc)
 
-    if len(valid_crops) == max_cellc:
-        save_crops(valid_crops, opimgpath, id)
+    valid_crops, mask_data = find_valid_crops(
+        image, masks, save_encode_mask, max_save=max_cellc
+    )
+
+    if len(valid_crops) == max_cellc or max_cellc == "all":
+        save_crops(valid_crops, opimgdir, id)
+        if save_encode_mask:
+            save_mask_data(mask_data, opcodedir, id)
     else:
         print(
-            f"Warning: Expected to save {max_cellc} cells, but found only {len(valid_crops)} valid cells for file {opimgpath.stem}"
+            f"Warning: Expected to save {max_cellc} cells, but found only {len(valid_crops)} valid cells for file {id}"
         )
     return
 
@@ -99,14 +127,20 @@ def crop_img(args):
     id_df = pd.read_csv(Path(args.ipcsv))
     id_list = id_df["ID"].to_list()
     n_CPU = args.n_CPU
-    opdir = Path(args.opdir)
-    opdir.mkdir(exist_ok=True, parents=True)
+    opimgdir = Path(args.opimgdir)
+    opimgdir.mkdir(exist_ok=True, parents=True)
+    if args.opcodedir is not None:
+        opcodedir = Path(args.opcode)
+        opcodedir.mkdir(exist_ok=True, parents=True)
+    else:
+        opcodedir = None
 
     _crop_img = partial(
         _crop_image,
         ipimgdir=Path(args.ipimgdir),
         ipsegdir=Path(args.ipsegdir),
-        opdir=opdir,
+        opimgdir=opimgdir,
+        opcodedir=opcodedir,
         max_cellc=args.max_cell_count,
     )
 
@@ -141,15 +175,21 @@ def main():
         "-is", "--ipsegdir", type=str, required=True, help="input seg dir"
     )
     required.add_argument(
-        "-o", "--opdir", type=str, required=True, help="output img dir"
+        "-o", "--opimgdir", type=str, required=True, help="output img dir"
     )
     # required.add_argument('-c', type=str, required=True, help='output csv dir')
     optional = parser.add_argument_group()
     optional.add_argument(
+        "--opcodedir", type=str, help="encode binary mask to coco challenge format"
+    )
+    optional.add_argument(
         "-n", "--n_CPU", type=int, default=1, help="number of CPU core to use"
     )
     optional.add_argument(
-        "--max_cell_count", type=int, default=10, help="max cell count per image"
+        "--max_cell_count",
+        type=str,
+        default="10",
+        help='max cell count per image. default = 10. use "all" to save all cells',
     )
     args = parser.parse_args()
     crop_img(args)
--- preprocessing/mike/preprocessing.sh
+++ preprocessing/mike/preprocessing.sh
@@ -1,77 +1,101 @@
 REPODIR="/dlab/ldrive/CBT/USLJ-DSDE-I10007/DSDE/shihch3/code/p/python/nova-networks"
 DATADIR="/dlab/ldrive/CBT/USLJ-DSDE_DATA-I10008/BenchmarkDatasets/hpa-single-cell-image-classification"
 OPDATADIR="/dlab/ldrive/CBT/USLJ-DSDE_DATA-I10008/shihch3/projects/HPA_single_data/code_test"
+CELLPOSE_CONDAENV="mscp03"
 
 SCRIPTDIR="${REPODIR}/preprocessing/mike"
 IPDIR="${DATADIR}/train"
 NCPU=10
 
-OPIMGDIR="${OPDATADIR}/train/img"
-OPIMGRGBDIR="${OPDATADIR}/train/imgRGB"
-OPSEGDIR="${OPDATADIR}/train/seg"
-OPCELLDIR="${OPDATADIR}/train/cell"
-CSVPATH="${REPODIR}/HPA-nova/dataloaders/split/train_sunni_toy.csv"
-FRAMESIZE=1024 # examples: "1024", "1024 1024", or "1024 512"
+INPUTTYPE="train valid"
+for type in $INPUTTYPE; do
 
-python "${SCRIPTDIR}/channel_merge.py" \
-    -i "${IPDIR}" \
-    -ic "${CSVPATH}" \
-    -o "${OPIMGDIR}" \
-    -n $NCPU \
-    -s $FRAMESIZE
+    OPIMGDIR="${OPDATADIR}/${type}/img"
+    OPIMGRGBDIR="${OPDATADIR}/${type}/imgRGB"
+    OPSEGDIR="${OPDATADIR}/${type}/seg"
+    OPCELLDIR="${OPDATADIR}/${type}/cell"
+    CSVPATH="${REPODIR}/HPA-nova/dataloaders/split/${type}_sunni_toy.csv"
+    FRAMESIZE=1024 # examples: "1024", "1024 1024", or "1024 512"
 
-python "${SCRIPTDIR}/channel_merge.py" \
-    -i "${IPDIR}" \
-    -ic "${CSVPATH}" \
-    -o "${OPIMGRGBDIR}" \
-    -n $NCPU \
-    -s $FRAMESIZE \
-    -m
+    # export 4ch png 
+    python "${SCRIPTDIR}/channel_merge.py" \
+        -i "${IPDIR}" \
+        -ic "${CSVPATH}" \
+        -o "${OPIMGDIR}" \
+        -n $NCPU \
+        -s $FRAMESIZE
 
-source activate mscp03
-python "${SCRIPTDIR}/cellpose_seg.py" \
-    -i "${OPIMGRGBDIR}" \
-    -o "${OPSEGDIR}"
-conda deactivate
+    # export 3ch RGB png
+    python "${SCRIPTDIR}/channel_merge.py" \
+        -i "${IPDIR}" \
+        -ic "${CSVPATH}" \
+        -o "${OPIMGRGBDIR}" \
+        -n $NCPU \
+        -s $FRAMESIZE \
+        -m # merge the red and yellow
 
+    # create segmentation using cellpose
+    # run with another conda env
+    source activate ${CELLPOSE_CONDAENV} 
+    python "${SCRIPTDIR}/cellpose_seg.py" \
+        -i "${OPIMGRGBDIR}" \
+        -o "${OPSEGDIR}"
+    conda deactivate
+
+    python "${SCRIPTDIR}/crop_img.py" \
+        -ic "${CSVPATH}" \
+        -ii "${OPIMGDIR}" \
+        -is "${OPSEGDIR}" \
+        -o "${OPCELLDIR}" \
+        -n $NCPU
+        # --max_cell_count 20 # <- default is 10 
+done
+
+# OPIMGDIR="${OPDATADIR}/val/img"
+# OPIMGRGBDIR="${OPDATADIR}/val/imgRGB"
+# OPSEGDIR="${OPDATADIR}/val/seg"
+# OPCELLDIR="${OPDATADIR}/val/cell"
+# CSVPATH="${REPODIR}/HPA-nova/dataloaders/split/valid_sunni_toy.csv"
+# FRAMESIZE=1024 # examples: "1024", "1024 1024", or "1024 512"
+# 
+# python "${SCRIPTDIR}/channel_merge.py" \
+#     -i "${IPDIR}" \
+#     -ic "${CSVPATH}" \
+#     -o "${OPIMGDIR}" \
+#     -n $NCPU \
+#     -s $FRAMESIZE
+# 
+# python "${SCRIPTDIR}/channel_merge.py" \
+#     -i "${IPDIR}" \
+#     -ic "${CSVPATH}" \
+#     -o "${OPIMGRGBDIR}" \
+#     -n $NCPU \
+#     -s $FRAMESIZE \
+#     -m
+# 
+# source activate ${CELLPOSE_CONDAENV} 
+# python "${SCRIPTDIR}/cellpose_seg.py" \
+#     -i "${OPIMGRGBDIR}" \
+#     -o "${OPSEGDIR}"
+# conda deactivate
+# 
+# python "${SCRIPTDIR}/crop_img.py" \
+#     -ic "${CSVPATH}" \
+#     -ii "${OPIMGDIR}" \
+#     -is "${OPSEGDIR}" \
+#     -o "${OPCELLDIR}" \
+#     -n $NCPU
+#     # --max_cell_count 20 # <- default is 10 
+
+# ========================================================================================
+
+# use "all" for --max_cell_count can output all single cells. 
+# Here is the example:  
+OPCELLALLDIR="${OPDATADIR}/val/cell_all"
 python "${SCRIPTDIR}/crop_img.py" \
     -ic "${CSVPATH}" \
     -ii "${OPIMGDIR}" \
     -is "${OPSEGDIR}" \
-    -o "${OPCELLDIR}" \
-    -n $NCPU
-
-OPIMGDIR="${OPDATADIR}/val/img"
-OPIMGRGBDIR="${OPDATADIR}/val/imgRGB"
-OPSEGDIR="${OPDATADIR}/val/seg"
-OPCELLDIR="${OPDATADIR}/val/cell"
-CSVPATH="${REPODIR}/HPA-nova/dataloaders/split/valid_sunni_toy.csv"
-FRAMESIZE=1024 # examples: "1024", "1024 1024", or "1024 512"
-
-python "${SCRIPTDIR}/channel_merge.py" \
-    -i "${IPDIR}" \
-    -ic "${CSVPATH}" \
-    -o "${OPIMGDIR}" \
+    -o "${OPCELLALLDIR}" \
     -n $NCPU \
-    -s $FRAMESIZE
-
-python "${SCRIPTDIR}/channel_merge.py" \
-    -i "${IPDIR}" \
-    -ic "${CSVPATH}" \
-    -o "${OPIMGRGBDIR}" \
-    -n $NCPU \
-    -s $FRAMESIZE \
-    -m
-
-source activate mscp03
-python "${SCRIPTDIR}/cellpose_seg.py" \
-    -i "${OPIMGRGBDIR}" \
-    -o "${OPSEGDIR}"
-conda deactivate
-
-python "${SCRIPTDIR}/crop_img.py" \
-    -ic "${CSVPATH}" \
-    -ii "${OPIMGDIR}" \
-    -is "${OPSEGDIR}" \
-    -o "${OPCELLDIR}" \
-    -n $NCPU
+    --max_cell_count all
